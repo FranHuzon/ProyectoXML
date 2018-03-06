13685c04d72f609cfb15351e547ca307417f48f4
FranHuzon <huzonvillarfrancisojesus@gmail.com>
2018-03-06 20:12:51 +0100

    Añadidos algunos cambios, hay que corregir el for de la segunda función

--- funcionesXML.py
+++ funcionesXML.py
@@ -12,16 +12,25 @@ def nombre_consejerias():
 		lista.append(nombre.text.replace("\n\t\t\t",""))
 	for elem in lista:
 		lista2.append(elem.replace("\n\t\t",""))
-
 	return lista2
 
 
-print(nombre_consejerias())
+def contar_organismos():
 
+	doc=etree.parse('consejerias_madrid.xml')
 
+	consejerias=doc.findall("consejeria")
+	lista=[]
+	for i in consejerias:
+		contador=0
+		if i.find("organismo"):
+			numorganismo=i.findall("organismo")
+			for j in numorganismo:
+				lista.append(i.find("organismo/nombre").text)
+	return lista
 
+for i in nombre_consejerias():
+	print("-",i)
 
-
-
-
-
+for i in contar_organismos():
+	print(i,"->",j)
