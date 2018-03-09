f5883a64a6420a0d1ee62fce90180c2b4fa5c501
FranHuzon <huzonvillarfrancisojesus@gmail.com>
2018-03-09 18:37:24 +0100

    Realizada función filtra_datos_consejeria

--- funcionesXML.py
+++ funcionesXML.py
@@ -37,8 +37,24 @@ def filtra_organismos(subcad):
 		
 	return lista
 
+def filtra_datos_consejeria(subcad):
 
+	doc=etree.parse('consejerias_madrid.xml')
 
+	consejerias=doc.findall("consejeria")
+	lista=[]
+	lista2=[]
+	for i in consejerias:
+		if subcad==i.find("nombre").text.replace("\n","").replace("\t",""):
+			pedida=i.findall("organismo")
+			for j in pedida:
+				if j.find("responsable") is not None:
+					lista.append(j.find("nombre").text.replace("\n","").replace("\t",""))
+					lista2.append(j.find("responsable").text.replace("\n","").replace("\t",""))
+				else:
+					lista.append(j.find("nombre").text.replace("\n","").replace("\t",""))
+					lista2.append("0")
+	return zip(lista,lista2)
 
 for i in nombre_consejerias():
 	print("-",i)
@@ -60,3 +76,15 @@ subcad=input("Introduce una subcadena: ")
 print("Estos son los organismos que contienen la subcadena introducidad:")
 for i in filtra_organismos(subcad):
 	print("-",i)
+
+print()
+print("-"*50)
+print()
+
+subcad=input("Introduce una consejería: ")
+
+for i in filtra_datos_consejeria(subcad):
+	if i[1]=="0":
+		print("-",i[0],"->","Este organismo no tiene una persona como responsable")
+	else:
+		print("-",i[0],"->",i[1])
