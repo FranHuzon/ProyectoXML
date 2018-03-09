ca5fcac26626460f013459fc122846fce4430313
FranHuzon <huzonvillarfrancisojesus@gmail.com>
2018-03-09 20:10:40 +0100

    Separación de las funciones y del programa principal

---
+++ programaprincipal.py
@@ -0,0 +1,45 @@
+for i in nombre_consejerias():
+	print("-",i)
+
+print()
+print("-"*50)
+print()
+
+conteo=zip(nombre_consejerias(),contar_organismos())
+
+for i in conteo:
+	print("-",i[0],"->",i[1])
+
+print()
+print("-"*50)
+print()
+
+subcad=input("Introduce una subcadena: ")
+print("Estos son los organismos que contienen la subcadena introducidad:")
+for i in filtra_organismos(subcad):
+	print("-",i)
+
+print()
+print("-"*50)
+print()
+
+subcad=input("Introduce una consejería: ")
+
+for i in filtra_datos_consejeria(subcad):
+	if i[1]=="0":
+		print("-",i[0],"->","Este organismo no tiene una persona como responsable")
+	else:
+		print("-",i[0],"->",i[1])
+
+print()
+print("-"*50)
+print()
+
+consejeria=input("Introduce una consejería: ")
+centro=input("Introduce un centro: ")
+
+for i in datos_centro(consejeria,centro):
+	if i[3]=="0":
+		print("Los datos del centro",i[0],"son :", i[1],";",i[2],";","Este organismo no tiene una persona como responsable")
+	else:
+		print("Los datos del centro",i[0],"son :", i[1],";",i[2],";",i[3])
