661a3fded371a77ff526426345c8efe6d077e198
FranHuzon <huzonvillarfrancisojesus@gmail.com>
2018-03-15 08:13:22 +0100

    Corregido el bucle while para el Menú y terminado

--- programaprincipal.py
+++ programaprincipal.py
@@ -18,20 +18,20 @@ while opcion!="0":
 			print("-",i)
 
 
-	if opcion=="2":
+	elif opcion=="2":
 		conteo=zip(nombre_consejerias(),contar_organismos())
 		for i in conteo:
 			print("-",i[0],"->",i[1])
 
 
-	if opcion=="3":
+	elif opcion=="3":
 		subcad=input("Introduce una subcadena: ")
 		print("Estos son los organismos que contienen la subcadena introducidad:")
 		for i in filtra_organismos(subcad):
 			print("-",i)
 
 
-	if opcion=="4":
+	elif opcion=="4":
 		subcad=input("Introduce una consejería: ")
 
 		for i in filtra_datos_consejeria(subcad):
@@ -41,7 +41,7 @@ while opcion!="0":
 				print("-",i[0],"->",i[1])
 
 
-	if opcion=="5":
+	elif opcion=="5":
 		consejeria=input("Introduce una consejería: ")
 		centro=input("Introduce un centro: ")
 
@@ -50,10 +50,11 @@ while opcion!="0":
 				print("Los datos del centro",i[0],"son :", i[1],";",i[2],";","Este organismo no tiene una persona como responsable.")
 			else:
 				print("Los datos del centro",i[0],"son :", i[1],";",i[2],";",i[3])
-		break
+		
 
 	else:
-		print('''
+		print("Opción incorrecta")
+	print('''
 	1.Nombre de las consejerías de Madrid.
 	2.Número de organismos que tiene cada consejería.
 	3.Búsqueda de organismos por caracteres introducidos.
@@ -61,9 +62,4 @@ while opcion!="0":
 	5.Información concreta de un centro específico de una consejería.
 	0.SALIR.
 	''')
-		opcion=input("Escoge otra opción del menú: ")
-
-
-
-#Consejería de Presidencia, Justicia y Portavocía del Gobierno
-#Subdirección General de lo Consultivo y Asuntos Constitucionales
+	opcion=input("Escoge otra opción del menú: ")
